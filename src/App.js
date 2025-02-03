6048e86c5b2cd8702780bebb1d9d8b6757d2fba3
Richard Dawson <dawson86316@gmail.com>
2025-02-03 21:06:55 +0100

    modified body-container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Body from './container/body/body';
 
 function App() {
   return (
     <div className="App">
-     
+      <Body />
     </div>
   );
 }
---
+++ src/container/body/body.js
@@ -0,0 +1,11 @@
+import './body.scss';
+
+function Body() {
+  return (
+    <div className="body-container">
+        
+    </div>
+  );
+}
+
+export default Body;
